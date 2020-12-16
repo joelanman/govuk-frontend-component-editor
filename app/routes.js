74ba6fb8100731bb3ea35b262854789c311e46d1
Joe Lanman <joelanman@gmail.com>
2020-12-16 14:50:54 +0000

    Use design system code tabs

--- app/routes.js
+++ app/routes.js
@@ -3,22 +3,20 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
-router.get('/components/:name', function(request, response){
+router.get('/components/:name', function (request, response) {
+  var options
+  var name = request.params.name
 
-    var options
-    var name = request.params.name
-
-    try{
-        options = require('../node_modules/govuk-frontend/govuk/components/' + name + '/macro-options.json')
-    } catch (error){
-        response.status(404)
-        response.send(error)
-        return
-    }
-    response.locals.name = name
-    response.locals.options = options
-    response.render('editor')
-
+  try {
+    options = require('../node_modules/govuk-frontend/govuk/components/' + name + '/macro-options.json')
+  } catch (error) {
+    response.status(404)
+    response.send(error)
+    return
+  }
+  response.locals.name = name
+  response.locals.options = options
+  response.render('editor')
 })
 
 module.exports = router
