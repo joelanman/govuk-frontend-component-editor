70a8a9c4bbe2f281f8552da6b6be31bfff7f980f
Joe Lanman <joelanman@gmail.com>
2022-07-07 19:56:35 +0100

    work on multiple items (arrays)

--- app/routes.js
+++ app/routes.js
@@ -14,6 +14,25 @@ const componentNameToMacroName = componentName => {
 
 // Add your routes here - above the module.exports line
 
+// we need counters
+
+let counters = {}
+
+getCounter = function(name, option){
+  counter = counters?.[name]?.[option] || 1
+  return counter
+}
+
+incrementCounter = function(name, option){
+  if (!counters[name]){
+    counters[name] = {}
+  }
+  if (!counters[name][option]){
+    counters[name][option] = 1
+  }
+  counters[name][option]++
+}
+
 router.get('/components/:name', function (request, response) {
   var options
   var name = request.params.name
@@ -26,9 +45,16 @@ router.get('/components/:name', function (request, response) {
     response.send(error)
     return
   }
+
+  if (request.query['add-another']){
+    let option = request.query['add-another']
+    incrementCounter(name, option)
+  }
+
   response.locals.name = name
   response.locals.macroName = macroName
   response.locals.options = options
+  response.locals.getCounter = getCounter
   response.render('editor')
 })
 
